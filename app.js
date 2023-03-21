fe97dcd0d63108ffb910999a1e65978b78e81e9a
Dsumanth11 <sumanth.bittu100@gmail.com>
2023-03-21 07:47:33 +0530

    done with object propValues containing property and values

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ dotenv.config();
 const port=process.env.PORT || 3000;
 app.use(express.static('public'));
 
+
+//used for storing key value pairs of different properties and their values
+let propValuesObj={};
+
 const path=require("path");
 app.use(express.json());
-// given constant port if not working assgin with variable
+// given constant port if not working assgin with variable port
 app.listen(3000,()=>{
     console.log("Server is running at http://localhost:3000/");
 });
@@ -16,17 +20,21 @@ app.get("/",(request,response)=>{
     // like name etc
     // and then call procced to function
     var result = '';
+    propValuesObj={};
     const spawn=require("child_process").spawn;
     const process=spawn('python',['./vtcli.py',"https://www.facebook.com/"]);
     process.stdout.on('data',(data)=>{
         abc=data;
+        // data will be in dictionary format as we are getting it from python
+        // storing the data as string format
         result+=data.toString();
         const r1=result.slice(12,result.length-4);
         let resultarr=r1.split(", ");
         // console.log(resultarr);
 
-        //used for storing key value pairs of different properties and their values
-        let propValuesObj={};
+        // propValueObj used for storing key value pairs of different properties and their values
+        //
+        // now converting the string into javascript Object format
         for(let i=1;i<resultarr.length;i=i+2)
         {
             let property=resultarr[i-1].slice(2,resultarr[i-1].length-1);
